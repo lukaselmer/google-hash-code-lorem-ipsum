0aacd1188168341623c06cd64bdef5cf799bface
Lukas Elmer <lukas.elmer@renuo.ch>
2017-02-23 21:56:32 +0100

    Include preprocessing

--- video/main.rb
+++ video/main.rb
@@ -1,13 +1,14 @@
 require_relative 'parser'
 require_relative 'random_optimize'
 require_relative 'export'
+require_relative 'preprocessing'
 
 ['kittens', 'me_at_the_zoo', 'trending_today', 'videos_worth_spreading'].each do |dataset_name|
   data = Parser.new("video/datasets/#{dataset_name}.in")
   data.parse
 
-  optimizer = RandomOptimizer.new(data)
-  optimizer.run
+  Preprocessing.new(data).preprocess
+  RandomOptimizer.new(data).run
 
   Export.new(data.caches, dataset_name).generate_file
 end
