ca391df280f50029a280f486ea4e74cc49708f2d
Lukas Elmer <lukas.elmer@renuo.ch>
2017-02-23 21:59:57 +0100

    More global warming

--- video/random_optimize.rb
+++ video/random_optimize.rb
@@ -7,7 +7,7 @@ class RandomOptimizer
     @data.caches.each do |cache|
       @current_cache = cache
       @caching_failed = 0
-      while @caching_failed < 100
+      while @caching_failed < 10000
         video = cache.cache_connections.sample.endpoint.request_descriptions.sample.video
         if @current_cache.cache_size >= video.size && !@current_cache.videos.include?(video)
           @current_cache.videos << video
