6bccc439e778c318dcd10e554941552f77aea2b7
Lukas Elmer <lukas.elmer@renuo.ch>
2017-02-23 19:29:55 +0100

    bla

--- spec/video/parser_spec.rb
+++ spec/video/parser_spec.rb
@@ -15,4 +15,31 @@ RSpec.describe Parser do
     expect(parser.num_caches).to eq(3)
     expect(parser.cache_size).to eq(100)
   end
+
+  it 'parses the videos' do
+    expect(parser.videos).to eq([50, 50, 80, 30, 110])
+  end
+
+  it 'parses the first endpoint latency' do
+    expect(parser.endpoints[0].datacenter_latency).to eq(1000)
+  end
+
+  it 'parses the first endpoint caches' do
+    expect(parser.endpoints[0].caches.length).to eq(3)
+    expect(parser.endpoints[0].caches[0].index).to eq(0)
+    expect(parser.endpoints[0].caches[0].cache_latency).to eq(100)
+    expect(parser.endpoints[0].caches[1].index).to eq(2)
+    expect(parser.endpoints[0].caches[1].cache_latency).to eq(200)
+    expect(parser.endpoints[0].caches[2].index).to eq(1)
+    expect(parser.endpoints[0].caches[2].cache_latency).to eq(300)
+  end
+
+  it 'parses all endpoints incl caches' do
+    expect(parser.endpoints[1].caches.length).to eq(0)
+  end
+
+  it 'parses the request_descriptions' do
+    expect(parser.request_descriptions.length).to eq(4)
+  end
+
 end
--- video/parser.rb
+++ video/parser.rb
@@ -1,5 +1,9 @@
+Endpoint = Struct.new(:datacenter_latency, :caches)
+Cache = Struct.new(:index, :cache_latency)
+RequestDescription = Struct.new(:video, :endpoint, :num_requests)
+
 class Parser
-  attr_reader :videos, :caches, :endpoints
+  attr_reader :videos, :endpoints, :request_descriptions
   attr_reader :num_videos, :num_endpoints, :num_request_descriptions, :num_caches, :cache_size
 
   def initialize(path)
@@ -8,22 +12,44 @@ class Parser
 
   def parse
     parse_first_line
+    parse_videos
+    parse_endpoints
+    parse_request_descriptions
   end
+
 
   def parse_first_line
     line = @lines.shift.strip.split(' ').map(&:to_i)
     @num_videos, @num_endpoints, @num_request_descriptions, @num_caches, @cache_size = line
   end
 
-  # def parse_header(lines)
-  #   x = lines.shift.split(' ')
-  #   @min_number_of_ingredient = x[2].to_i
-  #   @max_cells = x[3].to_i
-  # end
-  #
-  # def parse_grid(lines)
-  #   @grid = lines.map do |row|
-  #     row.strip.split('').map(&:downcase).map(&:to_sym)
-  #   end
-  # end
+  def parse_videos
+    @videos = @lines.shift.strip.split(' ').map(&:to_i)
+  end
+
+  def parse_endpoints
+    @endpoints = []
+    num_endpoints.times do
+      parse_endpoint
+    end
+  end
+
+  def parse_endpoint
+    datacenter_latency, num_connected_caches = @lines.shift.strip.split(' ').map(&:to_i)
+    endpoint = Endpoint.new(datacenter_latency, [])
+    num_connected_caches.times do
+      line = @lines.shift.strip.split(' ').map(&:to_i)
+      endpoint.caches << Cache.new(*line)
+    end
+    @endpoints << endpoint
+  end
+
+  def parse_request_descriptions
+    @request_descriptions = []
+    @num_request_descriptions.times do
+      line = @lines.shift.strip.split(' ').map(&:to_i)
+
+      @request_descriptions << nil
+    end
+  end
 end
